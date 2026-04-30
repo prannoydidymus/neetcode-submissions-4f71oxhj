fdff547353cc225ecac6a5ac8d468f08749b29a8
J.PRANNOY DIDYMUS <prannoydidymus@gmail.com>
2026-04-30 18:21:06 +0530

    Add: non-cyclical-number - submission-4

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-4.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+while(n>9){
+    int sum = 0;
+    while(n >0){
+        sum += Math.pow(n%10,2);
+        n /= 10;
+    }
+    n = sum;
+}
+if(n == 1 || n == 7){
+    return true;
+}
+else{
+    return false;
+}
+}
+}
