836ce3a74df7cf99c34d0ccae6d004de5d66051c
J.PRANNOY DIDYMUS <prannoydidymus@gmail.com>
2026-07-22 18:38:40 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int major = 0;
+        int count = 0;
+        for(int num:nums){
+            if(count == 0){
+                major = num;
+            }
+            if(num == major){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return major;
+    }
+}
